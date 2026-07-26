3a00b8ff448d787ff0a1ccde263cbe530602c122
Esther2Great-ness <edafioghoresther8@gmail.com>
2026-07-27 00:46:51 +0100

    updated the app file

--- app.py
+++ app.py
@@ -1,33 +1,10 @@
-import os
-
 from flask import Flask, render_template
 
-app = Flask(
-    __name__,
-    template_folder="dts-web/templates",
-    static_folder="dts-web/static",
-)
-
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("login.html")
 
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    return render_template("login.html")
-
-
-@app.route("/register")
-def register():
-    return render_template("register.html")
-
-
-@app.route("/dashboard")
-def dashboard():
-    return render_template("dashboard.html")
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+    app.run(debug=True)
