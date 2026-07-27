385f3256b9573e494560cd8e333ead1c17e2d06d
Edafioghor Iruo-Oghene Esther <112874647+Esther2Great-ness@users.noreply.github.com>
2026-07-27 07:15:18 +0100

    Fix route definition for login endpoint

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
   return render_template("index.html")
-@app.route("login")
+@app.route("/login")
 def home():
   return render_template("login.html")
 @app.route("/login", methods=["GET", "POST"])
